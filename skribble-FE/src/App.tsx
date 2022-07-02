9466a2c3e7a6f7615387513443ad753c221986d2
Pratham Kumar <pratham.kumar.jr@gmail.com>
2022-07-02 21:09:17 +0530

    canvas component

--- skribble-FE/src/App.tsx
+++ skribble-FE/src/App.tsx
@@ -1,5 +1,96 @@
-function App() {
-  return <div className=" text-red-600">Hello Skribble Project</div>;
-}
+import React, { useEffect, useRef, useState } from "react";
+import Canvas from "./components/Canvas";
 
-export default App;
+interface Props {}
+
+const App: React.FC<Props> = (props) => {
+  const [drawing, setDrawing] = useState(false);
+  const [pencil, setPencil] = useState(0);
+  const [height, setHeight] = useState(window.innerHeight / 2);
+  const [width, setWidth] = useState(window.outerWidth / 2);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const onDrawing = (
+    context: CanvasRenderingContext2D,
+    sx: number,
+    sy: number,
+    cx: number,
+    cy: number
+  ) => {
+    if (!drawing) return;
+    if (pencil === 0) {
+      sx *= width;
+      sy *= height;
+      cx *= width;
+      cy *= height;
+
+      context.beginPath();
+      context.moveTo(sx, sy);
+      context.lineTo(cx, cy);
+      context.fillStyle = "black";
+      context.lineWidth = 3;
+      context.stroke();
+    } else if (pencil === 1) {
+      //eraser
+
+      cx *= width;
+      cy *= height;
+      context.fillStyle = "white";
+      context.fillRect(cx, cy, 20, 20);
+    }
+  };
+
+  const startDrawing = () => setDrawing(true);
+  const endDrawing = () => setDrawing(false);
+  const onExit = () => {
+    if (drawing) setDrawing(false);
+  };
+
+  const selectEraser = () => setPencil(1);
+  const selectPencil = () => setPencil(0);
+
+  useEffect(() => {
+    window.addEventListener("resize", (event: any) => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight / 2);
+    });
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  }, [containerRef.current]);
+
+  return (
+    <div
+      className="h-screen w-screen
+     "
+    >
+      <div
+        className="w-full h-96 mx-auto border-2 border-black rounded-md "
+        ref={containerRef}
+      >
+        <Canvas
+          onDraw={onDrawing}
+          onStart={startDrawing}
+          onStop={endDrawing}
+          onEnd={onExit}
+          className="bg-white w-full h-full"
+          height={height}
+          width={width}
+        />
+      </div>
+      <div className="mt-4 flex justify-center space-x-4 h-1/10">
+        <button className="border-2 px-2" onClick={selectPencil}>
+          Pencil
+        </button>
+        <button className="border-2 px-2" onClick={selectEraser}>
+          Eraser
+        </button>
+      </div>
+    </div>
+  );
+};
+
+App.defaultProps = {};
+
+export default React.memo(App);
---
+++ skribble-FE/src/components/Canvas.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from "react";
+
+interface Point {
+  X: number;
+  Y: number;
+}
+
+interface Props {
+  onDraw: (
+    context: CanvasRenderingContext2D,
+    sx: number,
+    sy: number,
+    cx: number,
+    cy: number
+  ) => void;
+  onStart: () => void;
+  onStop: () => void;
+  className?: string;
+  onEnd: () => void;
+  height: number;
+  width: number;
+}
+
+const Canvas: React.FC<Props> = ({
+  onStart,
+  onDraw,
+  onEnd,
+  onStop,
+  height,
+  width,
+  className,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [start, setStart] = useState<Point>({ X: 0, Y: 0 });
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.height = height;
+    canvas.width = width;
+    canvas.style.height = "100%";
+    canvas.style.width = "100%";
+  }, [width, height]);
+
+  const startDrawing = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const bound = canvas.getBoundingClientRect();
+    const normalizeX = offsetX / bound.width;
+    const normalizeY = offsetY / bound.height;
+    setStart({ X: normalizeX, Y: normalizeY });
+    onStart();
+  };
+
+  const finishDrawing = () => {
+    onEnd();
+  };
+
+  const draw = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext("2d");
+    const bound = canvas.getBoundingClientRect();
+    const normalizeX = offsetX / bound.width;
+    const normalizeY = offsetY / bound.height;
+    onDraw(context!, start.X, start.Y, normalizeX, normalizeY);
+    setStart({ X: normalizeX, Y: normalizeY });
+  };
+
+  const canvasLeave = () => {
+    onStop();
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseMove={draw}
+      onMouseUp={finishDrawing}
+      onMouseLeave={canvasLeave}
+      className={className}
+    ></canvas>
+  );
+};
+
+Canvas.defaultProps = {};
+
+export default React.memo(Canvas);
